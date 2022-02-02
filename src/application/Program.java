74dcc156c9dd46d001d727a5895c17d6dc20c896
Fabiano Cardoso <sl.biano28@gmail.com>
2022-02-02 16:02:06 -0300

    create 50% of project program.exe

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,39 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourceEmployee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		System.out.print("Enter number of employees: ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; n<i; i++) {
+			System.out.println("Employee #"+ i +" data:");
+			System.out.print("Outsourced (y/n)? ");
+			char os = sc.nextLine().charAt(0);
+			
+				if ( os == 'y' ) {
+					list.add(new OutsourceEmployee(null, null, null, null));
+				}
+				else {
+					list.add(new Employee(null, null, null));
+				}
+		}
+		
+		sc.close();
 
 	}
 
